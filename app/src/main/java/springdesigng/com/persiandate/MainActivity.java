39cd578f361cc1f5a0cd0836d33b2e318dd77d9c
mojtabadj <mojtaba.developer@gmail.com>
2016-07-12 10:39:50 -0800

    Persian Date Time Picker

---
+++ app/src/main/java/springdesigng/com/persiandate/MainActivity.java
@@ -0,0 +1,188 @@
+package springdesigng.com.persiandate;
+
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.mojtaba.materialdatetimepicker.Utils;
+import com.mojtaba.materialdatetimepicker.date.DatePickerDialog;
+import com.mojtaba.materialdatetimepicker.time.RadialPickerLayout;
+import com.mojtaba.materialdatetimepicker.time.TimePickerDialog;
+
+import java.util.Calendar;
+import java.util.Locale;
+import com.mojtaba.materialdatetimepicker.utils.PersianCalendar;
+
+public class MainActivity extends AppCompatActivity implements
+        TimePickerDialog.OnTimeSetListener,
+        DatePickerDialog.OnDateSetListener
+{
+    private TextView timeTextView;
+    private TextView dateTextView;
+    private CheckBox mode24Hours;
+    private CheckBox modeDarkTime;
+    private CheckBox modeDarkDate;
+    private CheckBox modeCustomAccentTime;
+    private CheckBox modeCustomAccentDate;
+    private CheckBox vibrateTime;
+    private CheckBox vibrateDate;
+    private CheckBox dismissTime;
+    private CheckBox dismissDate;
+    private CheckBox titleTime;
+    private CheckBox titleDate;
+    private CheckBox showYearFirst;
+    private CheckBox enableSeconds;
+    private CheckBox limitTimes;
+    private CheckBox limitDates;
+    private CheckBox highlightDates;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String languageToLoad  = "fa"; // your language
+        Locale locale = new Locale(languageToLoad);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+
+        setContentView(R.layout.activity_main);
+
+        // Find our View instances
+        timeTextView = (TextView)findViewById(R.id.time_textview);
+        dateTextView = (TextView)findViewById(R.id.date_textview);
+        Button timeButton = (Button)findViewById(R.id.time_button);
+        Button dateButton = (Button)findViewById(R.id.date_button);
+        mode24Hours = (CheckBox)findViewById(R.id.mode_24_hours);
+        modeDarkTime = (CheckBox)findViewById(R.id.mode_dark_time);
+        modeDarkDate = (CheckBox)findViewById(R.id.mode_dark_date);
+        modeCustomAccentTime = (CheckBox) findViewById(R.id.mode_custom_accent_time);
+        modeCustomAccentDate = (CheckBox) findViewById(R.id.mode_custom_accent_date);
+        vibrateTime = (CheckBox) findViewById(R.id.vibrate_time);
+        vibrateDate = (CheckBox) findViewById(R.id.vibrate_date);
+        dismissTime = (CheckBox) findViewById(R.id.dismiss_time);
+        dismissDate = (CheckBox) findViewById(R.id.dismiss_date);
+        titleTime = (CheckBox) findViewById(R.id.title_time);
+        titleDate = (CheckBox) findViewById(R.id.title_date);
+        showYearFirst = (CheckBox) findViewById(R.id.show_year_first);
+        enableSeconds = (CheckBox) findViewById(R.id.enable_seconds);
+        limitTimes = (CheckBox) findViewById(R.id.limit_times);
+        limitDates = (CheckBox) findViewById(R.id.limit_dates);
+        highlightDates = (CheckBox) findViewById(R.id.highlight_dates);
+
+        // check if picker mode is specified in Style.xml
+        modeDarkTime.setChecked(Utils.isDarkTheme(this, modeDarkTime.isChecked()));
+        modeDarkDate.setChecked(Utils.isDarkTheme(this, modeDarkDate.isChecked()));
+
+        // Show a timepicker when the timeButton is clicked
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                TimePickerDialog tpd = TimePickerDialog.newInstance(
+                        MainActivity.this,
+                        c.get(Calendar.HOUR_OF_DAY),
+                        c.get(Calendar.MINUTE),
+                        mode24Hours.isChecked()
+                );
+                tpd.setThemeDark(modeDarkTime.isChecked());
+                tpd.vibrate(vibrateTime.isChecked());
+                tpd.dismissOnPause(dismissTime.isChecked());
+                tpd.enableSeconds(enableSeconds.isChecked());
+                if (modeCustomAccentTime.isChecked()) {
+                    tpd.setAccentColor(Color.parseColor("#9C27B0"));
+                }
+                if (titleTime.isChecked()) {
+                    tpd.setTitle("TimePicker Title");
+                }
+                if (limitTimes.isChecked()) {
+                    tpd.setTimeInterval(2, 5, 10);
+                }
+                tpd.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialogInterface) {
+                        Log.d("TimePicker", "Dialog was cancelled");
+                    }
+                });
+                tpd.show(getFragmentManager(), "Timepickerdialog");
+            }
+        });
+
+        // Show a datepicker when the dateButton is clicked
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PersianCalendar now = new PersianCalendar();
+                DatePickerDialog dpd = DatePickerDialog.newInstance(
+                        MainActivity.this,
+                        now.getPersianYear(),
+                        now.getPersianMonth(),
+                        now.getPersianDay()
+                );
+                dpd.setThemeDark(modeDarkDate.isChecked());
+                dpd.vibrate(vibrateDate.isChecked());
+                dpd.dismissOnPause(dismissDate.isChecked());
+                dpd.showYearPickerFirst(showYearFirst.isChecked());
+                if (modeCustomAccentDate.isChecked()) {
+                    dpd.setAccentColor(Color.parseColor("#9C27B0"));
+                }
+                if (titleDate.isChecked()) {
+                    dpd.setTitle("DatePicker Title");
+                }
+                if (limitDates.isChecked()) {
+                    PersianCalendar[] dates = new PersianCalendar[13];
+                    for(int i = -6; i <= 6; i++) {
+                        PersianCalendar date = new PersianCalendar();
+                        date.add(PersianCalendar.MONTH, i);
+                        dates[i+6] = date;
+                    }
+                    dpd.setSelectableDays(dates);
+                }
+                if (highlightDates.isChecked()) {
+                    PersianCalendar[] dates = new PersianCalendar[13];
+                    for(int i = -6; i <= 6; i++) {
+                        PersianCalendar date = new PersianCalendar();
+                        date.add(PersianCalendar.WEEK_OF_YEAR, i);
+                        dates[i+6] = date;
+                    }
+                    dpd.setHighlightedDays(dates);
+                }
+                dpd.show(getFragmentManager(), "Datepickerdialog");
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        TimePickerDialog tpd = (TimePickerDialog) getFragmentManager().findFragmentByTag("Timepickerdialog");
+        DatePickerDialog dpd = (DatePickerDialog) getFragmentManager().findFragmentByTag("Datepickerdialog");
+
+        if(tpd != null) tpd.setOnTimeSetListener(this);
+        if(dpd != null) dpd.setOnDateSetListener(this);
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int second) {
+        String hourString = hourOfDay < 10 ? "0"+hourOfDay : ""+hourOfDay;
+        String minuteString = minute < 10 ? "0"+minute : ""+minute;
+        String secondString = second < 10 ? "0"+second : ""+second;
+        String time = "You picked the following time: "+hourString+"h"+minuteString+"m"+secondString+"s";
+        timeTextView.setText(time);
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        String date = "You picked the following date: "+dayOfMonth+"/"+(++monthOfYear)+"/"+year;
+        dateTextView.setText(date);
+    }
+}
